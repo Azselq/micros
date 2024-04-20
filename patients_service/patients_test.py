62f3ae91fdaaafb053afdb827f881ebde352fa32
Azselq <denok2002@gmail.com>
2024-04-20 07:51:47 +0300

    update

--- patients_service/patients_test.py
+++ patients_service/patients_test.py
@@ -20,12 +20,12 @@ def test_create_patient(base_url):
     assert data['name'] == "nameTest"
     assert data['age'] == 25
 
-# def test_read_patient(base_url, test_patient):
-#     patient_id = test_patient['id']
-#     full_url = f"{base_url}/patients/{patient_id}"
-#     response = requests.get(full_url)
-#     assert response.status_code == 200, response.text
-#     data = response.json()
-#     assert data['id'] == patient_id
-#     assert data['name'] == "testName"
-#     assert data['age'] == 30
+def test_read_patient(base_url, test_patient):
+    patient_id = test_patient['id']
+    full_url = f"{base_url}/patients/{patient_id}"
+    response = requests.get(full_url)
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data['id'] == patient_id
+    assert data['name'] == "testName"
+    assert data['age'] == 30
