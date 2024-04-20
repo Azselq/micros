ee27512ad6265b30f7c03d02e7006f515d7e6c9a
Azselq <denok2002@gmail.com>
2024-04-20 09:10:14 +0300

    update

--- patients_service/patients_test.py
+++ patients_service/patients_test.py
@@ -2,14 +2,15 @@ import requests
 import pytest
 
 base_url = "http://localhost:8000/"
+
 @pytest.fixture(scope='module')
-def test_patient(base_url):
+def test_patient():
     payload = {"name": "testName", "age": 30}
     response = requests.post(f"{base_url}/patients/", json=payload)
     assert response.status_code == 200, response.text
     return response.json()
 
-def test_create_patient(base_url):
+def test_create_patient():
     payload = {"name": "nameTest", "age": 25}
     response = requests.post(f"{base_url}/patients/", json=payload)
     assert response.status_code == 200, response.text
@@ -17,7 +18,7 @@ def test_create_patient(base_url):
     assert data['name'] == "nameTest"
     assert data['age'] == 25
 
-def test_read_patient(base_url, test_patient):
+def test_read_patient(test_patient):
     patient_id = test_patient['id']
     full_url = f"{base_url}/patients/{patient_id}"
     response = requests.get(full_url)
